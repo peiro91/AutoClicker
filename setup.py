5cdb5da0f5af3947d52a11f32f584be028359aef
peiro91 <82719938+peiro91@users.noreply.github.com>
2021-04-17 19:15:33 +0200

    Trial change

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ def is_clicked(x, y, button, pressed):
 def auto_click(click_number,X,Y):   #perform clicks base on initialize_setup return.
     for click in range(click_number):
         pyautogui.click(x=X, y=Y, interval=0.1)
-    pyautogui.alert('autoGUI finished ')
+    pyautogui.alert('autoGUI finished')
