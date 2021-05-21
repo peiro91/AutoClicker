fcc3cd3baf465591e03c277664efd1813c03ea4c
peiro91 <piero.porro@hotmail.com>
2021-05-21 17:30:42 +0200

    added multi click. Need to find a way to stop registering clicks pressing keyboard

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from pynput.mouse import Listener
+from pynput.keyboard import Listener as KeyboardListener
+from pynput.keyboard import Key
 import pyautogui,time
 
 def is_clicked(x, y, button, pressed):
@@ -9,4 +11,10 @@ def is_clicked(x, y, button, pressed):
 def auto_click(click_number,X,Y):   #perform clicks base on initialize_setup return.
     for click in range(click_number):
         pyautogui.click(x=X, y=Y, interval=0.1)
+    pyautogui.alert('autoGUI finished')
+
+def multi_click(click_number,X=[],Y=[]):   #perform clicks base on initialize_setup return.
+    for click in range(click_number):
+        for i in range(len(X)):
+            pyautogui.click(x=X[i], y=Y[i], interval=0.1)
     pyautogui.alert('autoGUI finished')
